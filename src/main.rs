272603a034e9d91bed82c27bbef5bc5d296f365f
bleuboat <3751541788@qq.com>
2026-09-06 20:32:36 +0800

    回退为 rust

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+mod wiki;
+
+use axum::{extract::Path, response::Html, routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use wiki::Wiki;
+
+async fn get_html(path: String) -> Html<String> {
+    let wiki = Wiki::new(path);
+    Html(wiki.render())
+}
+
+async fn start() -> Html<String> {
+    get_html("start".to_owned()).await
+}
+
+async fn page(Path(path): Path<String>) -> Html<String> {
+    get_html(path).await
+}
+
+#[tokio::main]
+async fn main() {
+    let mut app = Router::new();
+    app = app.route("/", get(start));
+    app = app.route("/{*path}", get(page));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("正在运行：http://localhost:3000");
+    println!("按 Ctrl+C 退出");
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
